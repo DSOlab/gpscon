152b59c4ed6392f0c39e736f19f2eefbb170518c
xanthos <xanthos@mail.ntua.gr>
2016-02-18 11:46:45 +0200

    added nas folder

--- nas/sync_nas.sh
+++ nas/sync_nas.sh
@@ -1,14 +1,26 @@
 #! /bin/bash
 
+##
 ##  Synchronize folders between raskolnikov and babylon. This scripts expects
-##+ that you can ssh from raskolnikov to babylon without using a pass, i.e.
-##+ babylon should have the pub key of babylon in .ssh/authorized_keys, in
-##+ the root's $HOME directory.
+##+ that you can ssh from raskolnikov to babylon without using a pass.
+##
+##  TODO
+##      1. log the process to a file
+##      2. provide exit status
+##
+##  Keep this script in sync with the one in /bin @ raskolnikov.
+##
+##  Feb-2016 xxx
+##
 
 SSH=/opt/openssh/bin/ssh
 RSYNC=/opt/bin/rsync
 PORT=2592
 BABYLON=147.102.106.135
+
+##  Transfer Tide Gauge Data (pub/tide-gauge/megisti/)
+## ----------------------------------------------------
+## ----------------------------------------------------
 REMOTE_SOURCE=~/../raid0/data/pub/tide-gauge/megisti/
 LOCAL_TARGET=/raid0/data/pub/tide-gauge/megisti/
 
@@ -16,4 +28,16 @@ LOCAL_TARGET=/raid0/data/pub/tide-gauge/megisti/
 ${RSYNC} -e "${SSH} -p ${PORT}" -av root@${BABYLON}:${REMOTE_SOURCE} ${LOCAL_TARGET}
 
 ##  These are 777 
+/bin/chmod -R 777 ${LOCAL_TARGET}
+
+##  Transfer GNSS Data (pub/gnss/data/daily/${YEAR} where
+##+ YEAR is the current year.
+## ----------------------------------------------------                                      
+## ----------------------------------------------------
+YEAR=$(date +%Y)
+
+REMOTE_SOURCE=~/../raid0/data/pub/gnss/data/daily/${YEAR}/
+LOCAL_TARGET=/raid0/data/pub/gnss/data/daily/${YEAR}/
+
+${RSYNC} -e "${SSH} -p ${PORT}" -av root@${BABYLON}:${REMOTE_SOURCE} ${LOCAL_TARGET}
 /bin/chmod -R 777 ${LOCAL_TARGET}
