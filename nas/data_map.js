77dbacd7320991ecf3f51d3adbe7d0253ac45697
xanthos <xanthos.papanikolaou@gmail.com>
2016-02-27 14:41:44 +0200

    trying to plot time-series

--- nas/data_map.js
+++ nas/data_map.js
@@ -1,9 +1,11 @@
 // Create the Google Map…
 var map = new google.maps.Map(d3.select("#map").node(), {
-    zoom: 8,
-    center: new google.maps.LatLng(38.5, 22.5),
+    zoom     : 7,
+    center   : new google.maps.LatLng(38.5, 22.5),
     mapTypeId: google.maps.MapTypeId.TERRAIN
 });
+
+var width = 700, height = 400;
 
 // Load the station data. When the data comes back, create an overlay.
 d3.json("akyr.json", function(error, data) {
@@ -14,9 +16,9 @@ d3.json("akyr.json", function(error, data) {
     // Add the container when the overlay is added to the map.
     overlay.onAdd = function() {
 
-      var layer = d3.select(this.getPanes().overlayLayer)
-                    .append("div")
-                    .attr("class", "stations");
+    var layer = d3.select(this.getPanes().overlayLayer)
+                  .append("div")
+                  .attr("class", "stations");
 
       // Draw each marker as a separate SVG element.
       // We could use a single SVG, but what size would it have?
@@ -43,7 +45,7 @@ d3.json("akyr.json", function(error, data) {
                 .attr("x", padding + 7)
                 .attr("y", padding)
                 .attr("dy", ".31em")
-                .text(function(d) { return d.value["mark_name_DSO"]; });
+                .text(function(d) { return d.value.info["official_name"]; });
 
           function transform(d) {
               console.log("d=",d);
@@ -52,19 +54,12 @@ d3.json("akyr.json", function(error, data) {
               return d3.select(this)
                        .style("left", (d.x - padding) + "px")
                        .style("top", (d.y - padding) + "px");
-          }
+          }; // transform
 
-      };
-    };
-
-    var tssvg = .selectAll("svg")
-                      .data(d3.entries(data))
-                      .each(transform) // update existing markers
-                      .enter()
-                      .append("svg")
-                      .each(transform)
-                      .attr("class", "marker");
+      }; // overlay.draw
+    }; // overlay.onAdd
 
   // Bind our overlay to the map…
   overlay.setMap(map);
-});
+
+}); // d3.json
